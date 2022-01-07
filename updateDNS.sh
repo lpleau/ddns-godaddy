01abdaf1d759b6fda068495fc64f42fcf759658d
lpleau <63933737+lpleau@users.noreply.github.com>
2022-01-06 18:08:06 -0800

    Update updateDNS.sh

--- updateDNS.sh
+++ updateDNS.sh
@@ -5,9 +5,9 @@ host=$2
 APIKey=$3
 APISecret=$4
 
-WanIP='curl -s "https://api.ipify.org"
+WanIP=`curl -s "https://api.ipify.org"
 
-GDIP='curl -s -X GET -H "Authorization: sso-key ${APIKey}:${APISecret}" "https://api.godaddy.com/v1/domains/${domain}/records/A/${host}" | cut -d'[' -f 2 | cut -d']' -f 1 | jq -r '.data'
+GDIP=`curl -s -X GET -H "Authorization: sso-key ${APIKey}:${APISecret}" "https://api.godaddy.com/v1/domains/${domain}/records/A/${host}" | cut -d'[' -f 2 | cut -d']' -f 1 | jq -r '.data'
 
 if [ "$WanIP |= "$GDIP" -a "$WanIP" != "" ]; then
   curl -s -X PUT "https://api.godaddy.com/v1/domains/${domain}/records/A/${host}" -H "Authorization: sso-key ${APIKey}:${APISecret}" -H "Content-Type: application/json" -d "[{\"data\": \"${WanIP}\"}]"
