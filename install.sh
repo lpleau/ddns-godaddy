9be59356b5efde43e332b83040636dd95d10fb70
lpleau <63933737+lpleau@users.noreply.github.com>
2022-01-06 22:08:39 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,9 @@
 
 sudo chmod 700 updateDNS.sh
 sudo chmod 700 domainLISTddns.sh
+sudo chmod 700 addDNSdomain.sh
 sudo mv updateDNS.sh /usr/local/bin/updatedns.sh
 sudo mv domainLISTddns.sh /usr/local/bin/domainLISTddns.sh
+sudo mv addDNSdomain.sh /usr/local/bin/addDNSdomain
 sudo apt install jq -y
+addDNSdomain
