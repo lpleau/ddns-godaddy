4c34e97645afdbf95edd33d5cb2aa3edaae33c5d
lpleau <63933737+lpleau@users.noreply.github.com>
2022-02-06 21:11:59 -0500

    Update ddns directory name

--- install.sh
+++ install.sh
@@ -6,18 +6,18 @@ sudo chmod 777 script/domainlist
 sudo chmod 777 script/ddns-godaddy
 
 #Move all files to correct placement
-sudo mkdir /usr/local/bin/ddns-godaddy
-sudo cp script/updateDNS /usr/local/bin/ddns-godaddy/updatedns
-sudo cp script/domainlist /usr/local/bin/ddns-godaddy/domainlist
+sudo mkdir /usr/local/bin/ddns
+sudo cp script/updateDNS /usr/local/bin/ddns/updatedns
+sudo cp script/domainlist /usr/local/bin/ddns/domainlist
 sudo cp script/ddns-godaddy /usr/local/bin/ddns-godaddy
-sudo cp script/.domainAPI /usr/local/bin/ddns-godaddy/.domainAPI
+sudo cp script/.domainAPI /usr/local/bin/ddns/.domainAPI
 
 #Install dependencies
 sudo apt install jq -y
 
 #Create cron job to keep DDNS updated
 crontab -l > mycron
-echo "*/5 * * * * /usr/local/bin/ddns-godaddy/domainlist" >> mycron
+echo "*/5 * * * * /usr/local/bin/ddns/domainlist" >> mycron
 crontab mycron
 rm mycron
 
