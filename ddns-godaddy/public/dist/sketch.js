4ad74ebbe27d35c42916b05ca58402cbed349f55
lpleau <63933737+lpleau@users.noreply.github.com>
2022-10-11 00:19:29 -0400

    Update sketch.js
    Change dashboard domains and subdomains count system

--- ddns-godaddy/public/dist/sketch.js
+++ ddns-godaddy/public/dist/sketch.js
@@ -6,25 +6,11 @@ function setup() {
 }
 
 function domData(data) {
-    var options = data;
-    for (var i = 0; i < options.length; i++) {
-        var opt = options[i];
-	var el = document.createElement("option");
-	el.textContent = opt;
-	el.value = opt;
-    let domQty = options.length;
+    let domQty = data.length;
     document.getElementById("domainQty").innerHTML = domQty + " Domains";
-    }
 }
 
 function hostData(data) {
-    var options = data;
-    for (var i = 0; i < options.length; i++) {
-        var opt = options[i];
-	var el = document.createElement("option");
-	el.textContent = opt;
-	el.value = opt;
-    let hostQty = options.length;
+    var hostQty = data.length;
     document.getElementById("hostQty").innerHTML = hostQty + " Sub-Domains";
-    }
 }
